77d29f3d83dbb8d9e621561a6ba429f09344ee55
Daniel Ran <danielr1735@gmail.com>
2018-06-29 04:49:41 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,17 +13,27 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@all = []
-
-  # def self.find_by_name(name)
-  #   @@all.detect{|a| a.name == name}
-  # end
-
   def initialize(name='')
     @name = name
     @songs = []
     self.class.all << self
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
+
+
+  # def self.find_by_name(name)
+  #   @@all.detect{|a| a.name == name}
+  # end
 
   # def self.all
   #   @@all
@@ -37,17 +47,6 @@ class Artist
   #   @@all.count
   # end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
   # def to_param
   #   name.downcase.gsub(' ', '-')
   # end
-
-end
--- lib/song.rb
+++ lib/song.rb
@@ -13,20 +13,20 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@all = []
+  # @@all = []
 
   def initialize(name='')
     @name = name
     self.class.all << self
   end
 
-  # def self.find_by_name(name)
-  #   @@all.detect{|a| a.name == name}
-  # end
-
   def artist=(artist)
     @artist = artist
   end
+
+    # def self.find_by_name(name)
+    #   @@all.detect{|a| a.name == name}
+    # end
 
   # def self.reset_all
   #   @@all.clear
